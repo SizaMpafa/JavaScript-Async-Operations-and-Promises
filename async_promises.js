5120a24194b6465521771a4e1c86fb7bd76bc4ec
bugsagweler <bugsagweler@gmail.com>
2025-11-14 16:22:02 +0200

    saygqibezela endlini

--- async_promises.js
+++ async_promises.js
@@ -4,30 +4,91 @@
 // - If userId is negative or zero, reject with an error
 // - If userId is positive, resolve with user data object
 // // TODO: Create a function that uses template literals for HTML generation
+
 function fetchUserData(){
-    const userData = {
+  const userData = {
+    id: 1,
+    userName: "Yerr",
+    userEmail: "yerryesesss@gmail.com",
+    registrationDate: "14 November 2026"
+  };
+  const userId = userData.id
+  return new Promise((resolve, reject)=>{
+    setTimeout(() => {
+      if (userId > 0) {
+        resolve(userData)
+      }else if(userId === 0){
+        reject(`⚠️ I am not requesting for your ID number, I have no affair with HOME AFFAIRS, ${userId} is not allowed`)
+      }
+      else{
+        reject(`❌ What are you high on, even at Home Affairs an ID doesn't include/contain a negative value, "${userId}" what is this now?`)
+      }
+    }, 1500);
+  })
+}
+
+
+
+// TODO: Create a Promise that simulates fetching user posts
+// - Should resolve after 1 second
+// - Return an array of post objects
+// - Each post should have: id, title, content, and userId
+// - If userId doesn't exist, reject with error
+const fetchUserPosts =  async () => {
+  try{
+    const user = await fetchUserData();
+    const posts = [
+      {
+        id: 1,
+        postTitle: "Ezitshisayo",
+        postContent: "Loooooo, nguZibeqengeshe waseNgqushwa",
+        postBelongs: user.id
+      },
+      {
+        id: 2,
+        postTitle: "Crossing The Bridge",
+        postContent: "Pluto My Planet",
+        postBelongs: user.id
+      },
+      {
         id: 3,
-        userName: "Yerr",
-        userEmail: "yerryesesss@gmail.com",
-        registrationDate: "14 November 2026"
-    };
-    const userId = userData.id
-    return new Promise((resolve, reject)=>{
-        setTimeout(() => {
-            if (userId > 0) {
-                resolve(userData)
-            }else if(userId === 0){
-              reject(`⚠️ I am not requesting for your ID number, I have no affair with HOME AFFAIRS, ${userId} is not allowed`)
-            }
-            else{
-                reject(`❌ What are you high on, even at Home Affairs an ID doesn't include/contain a negative value, "${userId}" what is this now?`)
-            }
-        }, 1500);
-    })
+        postTitle: "Mikhulu Production",
+        postContent: "Sikuphatheli ndaba zishushu zinjalo",
+        postBelongs: user.id
+      }
+    ]
+    
+      const readline = require("readline");
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+
+
+    const guessedUserId = await new Promise((resolve) => {
+      rl.question("Guess which user number has posts: ", (answer) => {
+        rl.close();
+        resolve(Number(answer));
+      });
+    });
+
+    const postUserId = user.postBelongs
+            return new Promise((resolve, reject)=>{
+              setTimeout(() => {
+                if (guessedUserId === null){
+                  reject(`User number ${guessedUserId} does not exist. We only have  ${user.length} and  that user's id is ${postUserId} , so we can't fetch posts for user number ${postUserId}?`)
+                } else{
+                  resolve(posts)
+                }  
+              }, 1000)
+            })
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function main() {
-  try {
+   try {
     const data = await fetchUserData();
     const userTempLit = `<label>name</label>
                          <input>${data.userName}</input>
@@ -36,7 +97,14 @@ async function main() {
                          <label>name</label>
                          <input>${data.registrationDate}</input>
     `
-    console.log("✅ User data fetched:", userTempLit); 
+  console.log("✅ User data fetched:", userTempLit); 
+  } catch (error) {
+    console.error(error);
+  }
+
+  try {
+    const post = await fetchUserPosts();
+    console.log("✅ User posts fetched:", post); 
   } catch (error) {
     console.error(error);
   }
@@ -44,11 +112,7 @@ async function main() {
 main();
 
 
-// TODO: Create a Promise that simulates fetching user posts
-// - Should resolve after 1 second
-// - Return an array of post objects
-// - Each post should have: id, title, content, and userId
-// - If userId doesn't exist, reject with error
+
 
 // TODO: Create a function that chains multiple Promises together
 // - First fetch user data
